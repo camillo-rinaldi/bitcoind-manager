ddd1bf34afc8fc29fdf71f484db5d55f0512ba77
Camillo Rinaldi <camillo@kxptech.com>
2023-08-23 22:42:55 -0300

    add: tailwind

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -1,4 +1,6 @@
 <script>
+    import {onMount} from 'svelte';
+
     let blockCount;
 
     async function getBlockCount() {
@@ -9,21 +11,48 @@
             params: []
         };
 
-        const response = await fetch('/api/bitcoind', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(rpcBody)
-        });
 
-        const data = await response.json();
+        try {
+            const response = await fetch('/api/bitcoind', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(rpcBody)
+            });
 
-        console.log(data);
+            const data = await response.json();
+            blockCount = data.result;
+        } catch (e) {
+            //
+        }
 
-        blockCount = data.result;
     }
+
+    let intervalId;
+
+    onMount(() => {
+        // Fetch the block count immediately upon mounting
+        getBlockCount();
+
+        // Set an interval to fetch the block count every 5 seconds
+        intervalId = setInterval(getBlockCount, 5000);
+
+        // Cleanup the interval when the component is destroyed
+        return () => {
+            clearInterval(intervalId);
+        };
+    });
 </script>
 
-<button on:click={getBlockCount}>Get Block Count</button>
-<p>Block Count: {blockCount}</p>
+<div class="min-h-screen flex items-center justify-center bg-gray-100">
+    <div class="bg-white p-8 rounded-xl shadow-md w-96">
+        <h1 class="text-2xl font-semibold mb-4">Bitcoin Block Count</h1>
+        <div class="flex items-center justify-between">
+            <p class="text-xl font-medium">Block Count: <span class="text-blue-500">{blockCount ?? "Loading..."}</span></p>
+            <button on:click={getBlockCount} class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-50">
+                Refresh
+            </button>
+        </div>
+    </div>
+</div>
