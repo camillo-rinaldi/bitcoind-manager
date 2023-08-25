b0801bb4bd9e6f2c08c90a84e18feb7885cd9b69
Camillo Rinaldi <camillo@kxptech.com>
2023-08-25 20:23:46 -0300

    .env password and user

--- src/routes/api/bitcoind/+server.ts
+++ src/routes/api/bitcoind/+server.ts
@@ -1,27 +1,26 @@
 import { json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
+import { PASSWORD, USER } from '$env/static/private';
+
 export const POST: RequestHandler = async ({ request }) => {
-    const rpcServer = "http://192.168.0.101:8332";
-    const user = "twin";
-    const password = "ish!AWE*&uwerui42@3(87";
-    const headers = {
-        'Content-Type': 'text/plain',
-        // @ts-ignore
-        'Authorization': `Basic ${Buffer.from(`${user}:${password}`).toString('base64')}`
-    };
+	const rpcServer = 'http://192.168.0.101:8332';
+	const headers = {
+		'Content-Type': 'text/plain',
+		// @ts-ignore
+		Authorization: `Basic ${Buffer.from(`${USER}:${PASSWORD}`).toString('base64')}`
+	};
 
-   const body = await request.json();
+	const body = await request.json();
 
-    console.log("Sending RPC request:", body);
+	console.log('Sending RPC request:', body);
 
+	const rpcResponse = await fetch(rpcServer, {
+		method: 'POST',
+		headers: headers,
+		body: JSON.stringify(body)
+	});
 
-    const rpcResponse = await fetch(rpcServer, {
-        method: 'POST',
-        headers: headers,
-        body: JSON.stringify(body),
-    });
+	const data = await rpcResponse.json();
 
-    const data = await rpcResponse.json();
-
-   return json(data);
-}
+	return json(data);
+};
